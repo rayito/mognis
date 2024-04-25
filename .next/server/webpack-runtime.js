41f96b5dcdeb4e610b886ba7287d36a0b89cc5b1
Eden Hernandez <edenhernandez.de@gmail.com>
2024-04-25 16:49:54 +0200

    Por favor

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -125,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("c29f92941bedacce")
+/******/ 		__webpack_require__.h = () => ("db5309b6ea9673cd")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
--- app/f-main-content.js
+++ app/f-main-content.js
@@ -14,7 +14,7 @@ export default function FamilyMainContent() {
       <iframe className='map' src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d368.643229693604!2d-3.6270982684711846!3d37.14729526220358!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd71fb849ef8ecd3%3A0xd975cab51f23148e!2sCam.%20de%20Churriana%2C%2044%2C%2018100%20Armilla%2C%20Granada!5e1!3m2!1ses!2ses!4v1714047332553!5m2!1ses!2ses"></iframe>
     </div>
     <p className='mb1'>
-      ¡Ojo! necesitamos un poco de información sobre ustedes. Por favor, rellena este cortito formulario para que podamos organizarlo todo lo mejor posible. 
+      ¡Ojo! necesitamos un poco de información sobre ustedes. Rellena este cortito formulario para que podamos organizarlo todo lo mejor posible. 
     </p>
     <p className='mb1 pregunticas'>
       <a target='_blank' href='https://forms.gle/X5SrFAjsLLtdwwra8'>&#128073;&#127996; Pregunticas &#128072;&#127996;</a>
