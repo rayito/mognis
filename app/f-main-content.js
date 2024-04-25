6adca032fcb2c58ea00d2c74b45ded0ee9d7957e
Eden Hernandez <edenhernandez.de@gmail.com>
2024-04-25 15:47:36 +0200

    Link fixed

--- app/f-main-content.js
+++ app/f-main-content.js
@@ -8,7 +8,7 @@ export default function FamilyMainContent() {
       Será la tarde del sábado <strong className='large-text'>22 de junio</strong> y empezaremos sobre las <strong className='large-text'>17:30</strong>.
     </p>
     <p className='mb2'>
-      La celebración será en la <a target='_blank' src='https://maps.app.goo.gl/NLrQRFFdAr36AcXf9'>Villa María Luisa</a>, Armilla, Granada.
+      La celebración será en la <a target='_blank' href='https://maps.app.goo.gl/NLrQRFFdAr36AcXf9'>Villa María Luisa</a>, Armilla, Granada.
     </p>
     <div className='map-block center mb2'>
       <iframe className='map' src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d368.643229693604!2d-3.6270982684711846!3d37.14729526220358!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd71fb849ef8ecd3%3A0xd975cab51f23148e!2sCam.%20de%20Churriana%2C%2044%2C%2018100%20Armilla%2C%20Granada!5e1!3m2!1ses!2ses!4v1714047332553!5m2!1ses!2ses"></iframe>
